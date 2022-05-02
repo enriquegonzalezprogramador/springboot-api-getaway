72b417b8d319e4cc34a2df7c3826a3cefd2ea68c
Gonzalez <junior.gonzalez@accenture.com>
2022-05-02 15:35:44 -0300

    first commit

---
+++ src/test/java/com/microservicesexample/springbootapigetaway/ApiGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicesexample.springbootapigetaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
